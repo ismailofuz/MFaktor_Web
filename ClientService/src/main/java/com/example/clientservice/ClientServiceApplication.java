2de19c74a4ccd38a329304af7e9e0ac67baa3977
JafarbekTurayev <jafarbek1997@gmail.com>
2022-04-26 10:59:08 +0500

    active MQ ishlatdik

--- ClientService/src/main/java/com/example/clientservice/ClientServiceApplication.java
+++ ClientService/src/main/java/com/example/clientservice/ClientServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
-@EnableCaching
+//@EnableCaching
+//@ComponentScan(basePackages = "com.example.*")
 public class ClientServiceApplication {
 
     public static void main(String[] args) {
