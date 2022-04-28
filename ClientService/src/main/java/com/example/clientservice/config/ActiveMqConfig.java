5e3e0241b42f9802f5c7a6ed725c073d81736d42
Sirojiddin Ismoilov <sirojiddin.ismoilov7181@gmail.com>
2022-04-28 08:36:49 +0500

    Active MQ

--- ClientService/src/main/java/com/example/clientservice/config/ActiveMqConfig.java
+++ ClientService/src/main/java/com/example/clientservice/config/ActiveMqConfig.java
@@ -1,9 +1,16 @@
 package com.example.clientservice.config;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.client.RestTemplate;
+
+import javax.jms.Queue;
+
 
 @Configuration
 @EnableJms
@@ -11,5 +18,30 @@ public class ActiveMqConfig {
 
     @Value("${activemq.broker.url}")
     private String brokerUrl;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+    //navbat
+    @Bean
+    public Queue queue() {
+        return new ActiveMQQueue("clientServiceQueue");
+    }
+
+    //connection
+    @Bean
+    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+        activeMQConnectionFactory.setBrokerURL(brokerUrl);
+        return activeMQConnectionFactory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        return new JmsTemplate(activeMQConnectionFactory());
+    }
 
 }
